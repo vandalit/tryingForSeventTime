78656b837de1b035dd3f90bf34ea1faf278d761a
Van <84937597+vandalit@users.noreply.github.com>
2024-10-11 23:27:51 -0300

    working!

--- src/components/FirebaseComponent.vue
+++ src/components/FirebaseComponent.vue
@@ -3,27 +3,27 @@
         <h1>Productos chilenos</h1>
         <ul>
             <li v-for="producto in productos" :key="producto.id">
-                {{producto.nombre}}
+                {{ producto.nombre }}
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-    import {getFirestore, collection, getDocs} from "firebase/firestore";
+    import { getFirestore, collection, getDocs } from "firebase/firestore";
     import firebaseApp from "../firebaseConfig";
 
     export default {
         name: "FirebaseComponent",
         data() {
             return {
-                productos: {}
+                productos: []
             }
         },
         async mounted() {
             try {
                 const db = getFirestore(firebaseApp);
-                const productosSnapshot = await getDocs(collection(db, productos))
+                const productosSnapshot = await getDocs(collection(db, "productos"))
                 productosSnapshot.forEach((doc) => {
                     this.productos.push(doc.data())
                 });
